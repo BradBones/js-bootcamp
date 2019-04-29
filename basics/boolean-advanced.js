a163e648626bc3fabf6181bdd85da22748d5e228
Niza <niza@Nizas-MacBook-Pro-2.local>
2019-04-29 12:16:16 -0700

    boolean-advanced

---
+++ basics/boolean-advanced.js
@@ -0,0 +1,27 @@
+let isAccountLocked = false;
+let userRole = 'user';
+
+if (isAccountLocked) {
+    console.log('Account is locked');
+} else if (userRole === 'admin') {
+    console.log('Welcome Admin!');
+} else {
+    console.log('Welcome Back!');
+};
+
+
+// Challenge area
+
+let temp = 45;
+
+// It is freeziong outside!
+// It is hot outside!
+// Go for it. It is pretty nice.
+
+if (temp <= 32) {
+    console.log('It is freeziong outside!');
+} else if (temp >= 110) {
+    console.log('It is hot outside!');
+} else {
+    console.log('Go for it. It is pretty nice.');
+};
--- basics/boolean.js
+++ basics/boolean.js
@@ -5,22 +5,33 @@
 // <= less than or equal to operator
 // >= greater than or equal to operator
 
-let temp = 31
-let isFreezing = temp <= 32;
+let temp = 180;
+//let isFreezing = temp <= 32;
 //console.log(isFreezing);
 
+if (temp <= 32) {
+    //console.log('It is freezing outside!');
+}
 
+if (temp >= 110) {
+    //console.log('It is way too hot outside!');
+}
 
 // Challenge area
 
 // Create an age variable set to your age
-let age = 32;
-// Calculate is child - if they are 7 or under
-let isChild = age <= 7;
-// Calculate is senior - if they are 65 or older
-let isSenior = age >= 65;
-// Print is child value
-console.log(`Is this person a child? ${isChild}`)
-// Print is senior value
-console.log(`Is this person a senior? ${isSenior}`)
+let age = 30;
+
+// If 7 or under, print message about child pricing
+// IF 65 or older, print message about senior discout
+
+if (age <= 7) {
+    console.log('Yay! You get a child discount');
+} else if (age >= 65) {
+    console.log('Congrats! You get a senior discount');
+} else {
+    console.log('Sorry, you have to pay full price.');
+};
+
+
 
