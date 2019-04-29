7bf0bf38e0f5028a106c4fcefa79087b18a0b4eb
Niza <niza@Nizas-MacBook-Pro-2.local>
2019-04-29 10:38:18 -0700

    variable rules

---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+// Rule 1. You can't define a variable more than once
+let petName = 'Hal';
+petName = 'Spot';
+
+console.log(petName);
+
+// 2. There are rules related to variable names
+let x = 2;
+let result = x + 4;
+
+// 3. Variable names cannot be reserved keywords
+let llet = 12;
