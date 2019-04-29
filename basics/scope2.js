847a88b6f3a2d08f0e8c2b2dce382086972a2f03
Niza <niza@Nizas-MacBook-Pro-2.local>
2019-04-29 13:26:41 -0700

    scope part 2

---
+++ basics/scope2.js
@@ -0,0 +1,22 @@
+// SCOPE DIAGRAM
+
+// Global ()
+    // Local ()
+        // Local
+    // Local
+
+   
+//let name = 'Jane';
+    
+if (true) {
+    //let name = 'Mike'
+
+    if (true) {
+        let name = 'Jenn'
+        console.log(name);
+    }
+}
+
+if (true) {
+    console.log(name);
+}
