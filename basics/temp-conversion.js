190d9db65633cf4df182d7895076de7d96ab6bf8
Niza <niza@Nizas-MacBook-Pro-2.local>
2019-04-29 11:09:13 -0700

    temp-conversion

---
+++ basics/temp-conversion.js
@@ -0,0 +1,12 @@
+let fahrenheit = 32; // 0c - 273.15k
+
+// calculate cesius and store in celsius variable
+let celsius = (fahrenheit - 32) / 1.8;
+// print value
+console.log(`${fahrenheit} degrees F = ${celsius} degrees C`);
+
+// calculate kelvin value and store in variable
+let kelvin = (fahrenheit + 459.67) / 1.8;
+// print that value
+console.log(`${fahrenheit} degrees F = ${kelvin} degrees K`);
+
