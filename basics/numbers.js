1d734be67c7e5932a3dfd0d121590b7f0ce76f54
Niza <niza@Nizas-MacBook-Pro-2.local>
2019-04-29 10:31:26 -0700

    numbers

---
+++ basics/numbers.js
@@ -0,0 +1,18 @@
+let x = 11;
+let num = (x + 1) * 2; // *My /Dear +Aunt -Sally
+
+//console.log(num);
+
+let age = 26;
+let dogYears = (age + 1) / 7;
+
+//console.log(dogYears);
+
+
+// Challenge area - score / max score * 100
+
+let studentScore =  18;
+let maxScore = 20;
+let percent = (studentScore / maxScore) * 100;
+
+console.log(percent);
