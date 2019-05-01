7f46e4af6d498027cd18dba50dd04230cca99322
Niza <booking@bladeandbone.com>
2019-05-01 15:43:55 -0700

    string methods

---
+++ objects/string-methods.js
@@ -0,0 +1,31 @@
+let name = '  Jen Smith ';
+
+// Length property
+console.log(name.length);
+
+// Convert to upper case method
+console.log(name.toUpperCase());
+
+// Convert to lower case method
+console.log(name.toLowerCase());
+
+// Includes method
+let password = 'abc123password098';
+console.log(password.includes('password'));
+
+// Trim method - removes white space from before and after the string
+console.log(name.trim());
+
+
+// Challenge area
+
+// isValidPassword
+// length is more than 8 - and it doesn't contain the word password
+let isValidPassword = function (pass) {
+    return pass.length > 8 && !pass.includes('password');
+}
+
+
+console.log(isValidPassword('asdfp'));
+console.log(isValidPassword('abc123!@hfjdks'));
+console.log(isValidPassword('asdfpdjkhfdkpassword84598934'));
