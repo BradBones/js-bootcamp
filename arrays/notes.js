3fc8b85ca9283fd25b64234270886941525b4ead
Niza <booking@bladeandbone.com>
2019-05-03 12:59:26 -0700

    forEach and loops

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,4 +1,42 @@
 const notes = ['Note 1', 'Note 2', 'Note 3'];
 
+// // Removes the last item in an array - Returns removed item
+// console.log(`${notes.pop()} is removed with .pop`);
+
+// // Add something to the end of an array
+// notes.push('Pushed Note');
+
+// // Shift method removes the first item in an array and returns it
+// console.log(`.shift removes ${notes.shift()}`);
+
+// // Unshift method adds an item to the deginin of an array
+// notes.unshift('First Item');
+
+// Splice method - Two arguments, a = index, b = num of items to be removed
+notes.splice(1, 1);
+// Another use case...
+// Use 0 as the 2nd argument to remove nothing and use a 3rd argument to add something new at that index.
+notes.splice(1, 0, 'Something new');
+// Another use case...
+// Remove and item with the 2nd argument and then add something in it's place
+notes.splice(2, 1, 'replaced item');
+
+notes[0] = 'another replaced item';
+
+// forEach method takes one argument (which is itself a function)
+notes.forEach(function (item, index) {
+    console.log(index);
+    console.log(item);
+});
+
 console.log(notes.length);
-console.log(notes[notes.length - 1]);
+console.log(notes);
+
+for (let i = 0; i <= 2; i++ ) {
+    console.log(i);
+}
+
+// Counting backwards with a for loop
+for (let count = notes.length -1; count >= 0; count--) {
+    console.log(notes[count]);
+}
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,4 +1,52 @@
 const todos = ['Walk the dog', 'go to the gym', 'cut Claire\'s hair', 'shoot a video', 'pick up cake'];
 
+// delete 3rd item
+todos.splice(2, 1);
+// add new item to the end
+todos.push('cut Jen\'s hair');
+// remove the first item from the list
+todos.shift();
+
+
 console.log(`You have ${todos.length} todos.`);
-console.log(`First todo: ${todos[0]} | Second to last todo: ${todos[todos.length - 2]}`);
+console.log(todos);
+
+// 1. first item
+// 2. second item
+// ...
+
+todos.forEach(function (item, index) {
+    console.log(`${index + 1}. ${item}`)
+})
+
+// For loop 
+for (let i = 0; i < todos.length; i++) {
+    const num = i + 1;
+    const todo = todos[i];
+    console.log(`For loop | ${num}. ${todos[i]}`);
+}
+
+// Reverse loop
+for (let i = todos.length -1; i >= 0; i--) {
+    const num = i + 1;
+    const todo = todos[i];
+    console.log(`Reverse loop | ${num}. ${todos[i]}`);
+}
+
+
+// MY CHALLENGE
+// Nums running forwards while list runs in reverse
+
+// Create a new empty array to store the reversed list
+let todoReverse = [];
+
+// Use a for loop to reverse the order of items in the original array
+// NOTE: When the items are fed into a new array in reverse they will be given a new index number [0, 1, 2, 3 ...]
+for (let i = todos.length -1; i >= 0; i--) {
+    todoReverse.push(todos[i]);
+}
+
+// Call .forEach on the new array and print the new index of each item
+todoReverse.forEach(function (item, index) {
+    console.log(`${index + 1}. ${item}`)
+})
