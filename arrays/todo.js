f705927cc8671f035644da6d787950cc0ebb4194
Niza <booking@bladeandbone.com>
2019-05-02 17:17:36 -0700

    Arrays basics

---
+++ arrays/todo.js
@@ -0,0 +1,4 @@
+const todos = ['Walk the dog', 'go to the gym', 'cut Claire\'s hair', 'shoot a video', 'pick up cake'];
+
+console.log(`You have ${todos.length} todos.`);
+console.log(`First todo: ${todos[0]} | Second to last todo: ${todos[todos.length - 2]}`);
