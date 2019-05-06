fc082b2f5b9a525b54a981b89587111e39583f99
Niza <booking@bladeandbone.com>
2019-05-06 16:20:04 -0700

    Adding elements via the DOM

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,30 @@
-console.log('This is from our JS file')
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Japan'
+}, {
+    title: 'Lifestyle improvments',
+    body: 'Excercise more. Eat less meat.'
+}, {
+    title: 'Office modification',
+    body: 'Get a new chair.'
+}];
+
+
+// DOM - Document Object Model
+
+// // Query and remove
+// const p = document.querySelector('p');
+// p.remove();
+
+// Query all and remove - querySelectorAll returns an array
+const ps = document.querySelectorAll('p');
+ps.forEach(function (p) {
+    p.textContent = '*******';
+    //console.log(p.textContent);
+    //p.remove();
+});
+
+// Add a new element
+const newParagraph = document.createElement('p');
+newParagraph.textContent = 'This is a new element from JS';
+document.querySelector('body').appendChild(newParagraph);
---
+++ todo-app/todo-app.js
@@ -0,0 +1,56 @@
+const todos = [{
+    todo: 'Walk the dog', 
+    completed: false
+}, {
+    todo: 'go to the gym',
+    completed: true
+}, {
+    todo: 'cut Claire\'s hair',
+    completed: true
+}, {
+    todo: 'shoot a video',
+    completed: true
+}, {
+    todo: 'pick up cake',
+    completed: false
+}];
+
+// You have -- todos left (p element)
+
+// Filter completed todos and return the objects in a new array
+const incompleteTodos = todos.filter(function (todo) {
+    return !todo.completed
+});
+
+// Creating a p element to display the number of pending todos in the DOM
+const summary = document.createElement('h3');
+summary.textContent = `You have ${incompleteTodos.length} todos left.`;
+document.querySelector('body').appendChild(summary);
+
+
+// Add a p for each todo above (use text value)
+todos.forEach(element => {
+    const showPendingTodos = document.createElement('p');
+    showPendingTodos.textContent = element.todo;
+    document.querySelector('body').appendChild(showPendingTodos);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const p = document.querySelectorAll('p');
+
+// p.forEach(paragraph => {
+//     if (paragraph.textContent.includes('the')) {
+//         paragraph.remove();
+//     }
+// });
