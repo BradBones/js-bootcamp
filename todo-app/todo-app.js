f515376b5bdfb33eb315f5e6b9bef9ac1ce439e1
Niza <booking@bladeandbone.com>
2019-05-08 14:09:49 -0700

    Adding filter function to todos app

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -15,25 +15,41 @@ const todos = [{
     completed: false
 }];
 
-// You have -- todos left (p element)
 
-// Filter completed todos and return the objects in a new array
-const incompleteTodos = todos.filter(function (todo) {
-    return !todo.completed
-});
-
-// Creating a p element to display the number of pending todos in the DOM
-const summary = document.createElement('h3');
-summary.textContent = `You have ${incompleteTodos.length} todos left.`;
-document.querySelector('body').appendChild(summary);
+const filters = {
+    searchText: ''
+};
 
 
-// Add a p for each todo above (use text value)
-todos.forEach(element => {
-    const showPendingTodos = document.createElement('p');
-    showPendingTodos.textContent = element.todo;
-    document.querySelector('body').appendChild(showPendingTodos);
-});
+const renderTodos = function (todos, filters) {
+    const filteredTodos = todos.filter(function (item) {
+        return item.todo.toLowerCase().includes(filters.searchText.toLowerCase());
+    })
+
+    // Filter completed todos and return the objects in a new array
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed
+    });
+
+    // Clear the div after calculating things and before rendering things.
+    document.querySelector('#todos').innerHTML = '';
+
+    // Creating a p element to display the number of pending todos in the DOM
+    const summary = document.createElement('h3');
+    summary.textContent = `You have ${incompleteTodos.length} todos left.`;
+    document.querySelector('#todos').appendChild(summary);
+
+
+    // Add a p for each todo above (use text value)
+    filteredTodos.forEach(element => {
+        const showPendingTodos = document.createElement('p');
+        showPendingTodos.textContent = element.todo;
+        document.querySelector('#todos').appendChild(showPendingTodos);
+    });
+};
+
+renderTodos(todos, filters);
+
 
 // Listen for new todo creation
 document.querySelector('#add-todo').addEventListener('click', function (e){
@@ -45,21 +61,9 @@ document.querySelector('#new-todo-text').addEventListener('input', function (e) 
     console.log(e.target.value);
 });
 
-
-
-
-
-
-
-
-
-
-// const p = document.querySelectorAll('p');
-
-// p.forEach(paragraph => {
-//     if (paragraph.textContent.includes('the')) {
-//         paragraph.remove();
-//     }
-// });
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value;
+    renderTodos(todos, filters);
+});
 
 
